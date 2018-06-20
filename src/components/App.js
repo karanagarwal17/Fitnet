4eccd79a370199f9fa84dcdee8af6ed0db75e619
karanagarwal17 <karanagarwal1702@gmail.com>
2018-06-20 11:55:22 +0530

    add app component

---
+++ src/components/App.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import agent from '../agent'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+
+import {
+  APP_LOAD,
+  REDIRECT
+} from '../constants/actionTypes'
+
+const mapStateToProps = state => ({
+  ...state.common,
+  currentUser: state.common.currentUser,
+  redirectTo: state.common.redirectTo
+})
+
+const mapDispatchToProps = dispatch => ({
+  onLoad: (payload, token) =>
+    dispatch({ type: APP_LOAD, payload, token}),
+  onRedirect: () =>
+    dispatch({ type: REDIRECT })
+})
+
+class App extends React.Component {
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.redirectTo) {
+      this.context.router.replace(nextProps.redirectTo)
+      this.props.onRedirect()
+    }
+  }
+
+  componentWillMount() {
+    const token = window.localStorage.getItem('jwt')
+    if(token) {
+      agent.setToken(token)
+    }
+    this.props.onLoad(token ? agent.Auth.current() : null, token)
+  }
+
+  render(){
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+App.contextTypes = {
+  router: PropTypes.object.isRequired
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
