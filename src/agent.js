794a96feb2e045e047a15e051f4e3384690be411
karanagarwal17 <karanagarwal1702@gmail.com>
2018-06-21 14:59:49 +0530

    make api url dynamic

--- src/agent.js
+++ src/agent.js
@@ -1,9 +1,11 @@
 import superagentPromise from 'superagent-promise'
 import _superagent from 'superagent'
 
+import { backendAPI } from './.env.js'
+
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'http://localhost:3000/api'
+const API_ROOT = backendAPI
 
 const responseBody = res => res.body
 
