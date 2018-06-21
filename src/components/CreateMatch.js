7c928c7fed66b1ffbe33b68007d58e724142c304
karanagarwal17 <karanagarwal1702@gmail.com>
2018-06-21 14:45:50 +0530

    add create match

---
+++ src/components/CreateMatch.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class CreateMatch extends React.Component {
+    render(){
+        return(
+            <div>
+                CreateMatch here
+            </div>
+        )
+    }
+}
+
+export default CreateMatch
