2a7c83792359661a127b8c30e7d9f39ec998b8ac
karanagarwal17 <karanagarwal1702@gmail.com>
2018-06-21 14:45:32 +0530

    modify index of react

--- src/index.js
+++ src/index.js
@@ -3,30 +3,28 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
-import agent from './agent'
 import App from './components/App'
-import AddEvent from './components/AddEvent'
-import Donate from './components/Donate'
-import Donations from './components/Donations'
-import Home from './components/Home'
+import CreateMatch from './components/CreateMatch'
 import EditProfile from './components/EditProfile'
-import NgoProfile from './components/Profile/NgoProfile'
-import VolunteerProfile from './components/Profile/VolunteerProfile'
-import Search from './components/Search'
+import Feed from './components/Feed'
+import FindMatch from './components/FindMatch'
+import FindPlayers from './components/FindPlayers'
+import FindVenue from './components/FindVenue'
+import Profile from './components/Profile'
 import store from './store'
 
 ReactDOM.render((
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
-        <IndexRoute component={Home} />
-        <Route path="edit" component={EditProfile} />
-        <Route path="donations" component={Donations} />
-        <Route path="donate/:id" component={Donate} />
-        <Route path="profile/ngo/:id" component={NgoProfile} />
-        <Route path="profile/volunteer/:id" component={VolunteerProfile} />
-        <Route path="addevent" component={AddEvent} />
-        <Route path="search" component={Search} />
+        <IndexRoute component={Feed} />
+        <Route path="create/match" component={CreateMatch} />
+        <Route path="find/match" component={FindMatch} />
+        <Route path="find/players" component={FindPlayers} />
+        <Route path="find/venue" component={FindVenue} />
+        <Route path="edit/profile" component={EditProfile} />
+        <Route path="profile" component={Profile} />
+        <Route path="profile/:username" component={Profile} />
       </Route>
     </Router>
   </Provider>
