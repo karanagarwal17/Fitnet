1a22661a62cf5ab25288841914ed73d84c25f465
karanagarwal17 <karanagarwal1702@gmail.com>
2018-06-22 17:32:17 +0530

    update index router

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
+const match = require('./match');
 const user = require('./user');
+const venue = require('./venue');
 
+router.use('/match', match);
 router.use('/users', user);
+router.use('/venue', venue);
 
 module.exports = router;
