1b53ce7534bf3a227a84b9499c1b3389afdebdbd
Karan Agarwal <karanagarwal@Karans-MacBook-Pro.local>
2018-06-23 00:05:11 +0530

    implement find players

--- src/components/FindPlayers.js
+++ src/components/FindPlayers.js
@@ -1,13 +1,44 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import agent from '../agent'
+import PlayerCard from './Cards/PlayerCard'
+import {
+    PLAYER_SEARCH
+} from '../constants/actionTypes'
+
+const mapStateToProps = state => ({
+    ...state.player
+})
+
+const mapDispatchToProps = dispatch => ({
+    onLoad: () =>
+        dispatch({ type: PLAYER_SEARCH, payload: agent.Player.get() })
+})
 
 class FindPlayers extends React.Component {
+    componentWillMount(){
+        this.props.onLoad()
+    }
+
     render(){
-        return(
-            <div>
-                FindPlayers here
-            </div>
-        )
+        console.log(this.props.players)
+        if(this.props.players){
+            return(
+                <div>
+                    {
+                        this.props.players.map((player, key) => {
+                            return(
+                                <PlayerCard key={key} data={player} />
+                            )
+                        })
+                    }
+                </div>
+            )
+        } else {
+            return null
+        }
     }
 }
 
-export default FindPlayers
+export default connect(mapStateToProps, mapDispatchToProps)(FindPlayers)
