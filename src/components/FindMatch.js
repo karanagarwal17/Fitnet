2d0ca65d4cf30def43e7179ea0154aca6dd575ba
Karan Agarwal <karanagarwal@Karans-MacBook-Pro.local>
2018-06-23 00:05:00 +0530

    implement find match

--- src/components/FindMatch.js
+++ src/components/FindMatch.js
@@ -1,13 +1,43 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import agent from '../agent'
+import MatchCard from './Cards/MatchCard'
+import {
+    MATCH_SEARCH
+} from '../constants/actionTypes'
+
+const mapStateToProps = state => ({
+    ...state.match
+})
+
+const mapDispatchToProps = dispatch => ({
+    onLoad: () =>
+        dispatch({ type: MATCH_SEARCH, payload: agent.Match.get() })
+})
 
 class FindMatch extends React.Component {
+    componentWillMount(){
+        this.props.onLoad()
+    }
+
     render(){
-        return(
-            <div>
-                FindMatch here
-            </div>
-        )
+        if(this.props.matches){
+            return(
+                <div>
+                    {
+                        this.props.matches.map((match, key) => {
+                            return(
+                                <MatchCard data={match} key={key} />
+                            )
+                        })
+                    }
+                </div>
+            )
+        } else {
+            return null
+        }
     }
 }
 
-export default FindMatch
+export default connect(mapStateToProps, mapDispatchToProps)(FindMatch)
