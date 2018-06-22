8ce68e9d9c45434ff81f80956080cbc1397d79c6
Karan Agarwal <karanagarwal@Karans-MacBook-Pro.local>
2018-06-23 00:05:19 +0530

    implement find venue

--- src/components/FindVenue.js
+++ src/components/FindVenue.js
@@ -1,13 +1,41 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import agent from '../agent'
+import VenueCard from './Cards/VenueCard'
+import {
+    VENUE_SEARCH
+} from '../constants/actionTypes'
+
+const mapStateToProps = state => ({
+    ...state.venue
+})
+
+const mapDispatchToProps = dispatch => ({
+    onLoad: () =>
+        dispatch({ type: VENUE_SEARCH, payload: agent.Venue.get() })
+})
 
 class FindVenue extends React.Component {
+    componentWillMount(){
+        this.props.onLoad()
+    }
+    
     render(){
-        return(
-            <div>
-                FindVenue here
-            </div>
-        )
+        if(this.props.venues){
+            return(
+                <div>
+                    {
+                        this.props.venues.map((venue, key) => {
+                            <VenueCard key={key} data={venue} />
+                        })
+                    }
+                </div>
+            )
+        } else {
+            return null
+        }
     }
 }
 
-export default FindVenue
+export default connect(mapStateToProps, mapDispatchToProps)(FindVenue)
